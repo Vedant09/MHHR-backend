743cffaf04a771bb978f08dac88d836f3d89c70d
Vedant09 <vedantvy98@gmail.com>
2024-02-12 15:28:30 -0500

    filter

--- routes/properties/index.js
+++ routes/properties/index.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 const getAllProperties = require('./getAllPropertiesList');
 const addProperty = require('./addProperty');
 const getDetails = require('./getDetails');
+const filterDetails = require('./filterDetails');
 
 
 router.get('/get-all-properties', getAllProperties);
 router.post('/add-property', addProperty);
-router.get('/get-details/:id', getDetails)
+router.get('/get-details/:id', getDetails);
+router.get('/get-filter-details/:bedrooms/:bathrooms/:houseType', filterDetails);
 
 
 module.exports = exports = router;
