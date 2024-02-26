e1d64d034857dfbbc9429b9bf4712156ac8f4946
Vedant09 <vedantvy98@gmail.com>
2024-02-25 21:28:46 -0500

    filter changes pt1

--- routes/properties/filterDetails.js
+++ routes/properties/filterDetails.js
@@ -1,48 +1,54 @@
 const models = require("../../models");
 
 async function filterDetails(req, res, next) {
-    try {
-        const { bedrooms, bathrooms, houseType } = req.params;
-        let filteredProperties = {};
-        console.log(houseType);
-        let filter = {};
-        
-        if (!bathrooms || !bedrooms) {
-            return res.status(400).json({
-                message: 'Both bedrooms and bathrooms are required parameters.',
-            });
-        }
-        
-        if (bathrooms == 0 || bedrooms == 0) {
-            return res.status(409).json({
-                message: 'Bedrooms or bathrooms cannot be zero.',
-            });
-        }
-        
-        if (houseType && houseType !== 'apartment' && houseType !== 'condo' && houseType !== 'town_house' && houseType != 'single_family') {
-            return res.status(400).json({
-                message: 'Invalid house type. Valid values are "apartment", "single_family", "condo", or "town_house".',
-            });
-        }
-        
-        filter.bedrooms = bedrooms;
-        filter.bathrooms = bathrooms;
-        
-        if (houseType) {
-            filter.houseType = houseType;
-        }
-        
-        filteredProperties = await models.Property.findAll({
-            where: filter
-        });
-        
-        return res.status(200).json({
-            data: filteredProperties,
-        });
-    } catch (error) {
-        console.log(error, "error");
-        return next(error);
+  try {
+    const { bedrooms, bathrooms, houseType } = req.params;
+    let filteredProperties = {};
+    console.log(houseType);
+    let filter = {};
+
+    if (!bathrooms || !bedrooms) {
+      return res.status(400).json({
+        message: "Both bedrooms and bathrooms are required parameters.",
+      });
     }
+
+    if (bathrooms == 0 || bedrooms == 0) {
+      return res.status(409).json({
+        message: "Bedrooms or bathrooms cannot be zero.",
+      });
+    }
+    if (
+      houseType &&
+      houseType !== "apartment" &&
+      houseType !== "condo" &&
+      houseType !== "town_house" &&
+      houseType != "single_family"
+    ) {
+      return res.status(400).json({
+        message:
+          'Invalid house type. Valid values are "apartment", "single_family", "condo", or "town_house".',
+      });
+    }
+
+    filter.bedrooms = bedrooms;
+    filter.bathrooms = bathrooms;
+
+    if (houseType) {
+      filter.houseType = houseType;
+    }
+
+    filteredProperties = await models.Property.findAll({
+      where: filter,
+    });
+
+    return res.status(200).json({
+      data: filteredProperties,
+    });
+  } catch (error) {
+    console.log(error, "error");
+    return next(error);
+  }
 }
 
 module.exports = exports = filterDetails;
--- routes/properties/index.js
+++ routes/properties/index.js
@@ -1,15 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const getAllProperties = require('./getAllPropertiesList');
-const addProperty = require('./addProperty');
-const getDetails = require('./getDetails');
-const filterDetails = require('./filterDetails');
+const getAllProperties = require("./getAllPropertiesList");
+const addProperty = require("./addProperty");
+const getDetails = require("./getDetails");
+const filterDetails = require("./filterDetails");
 
-
-router.get('/get-all-properties', getAllProperties);
-router.post('/add-property', addProperty);
-router.get('/get-details/:id', getDetails);
-router.get('/get-filter-details/:bedrooms/:bathrooms/:houseType', filterDetails);
-
+router.get("/get-all-properties", getAllProperties);
+router.post("/add-property", addProperty);
+router.get("/get-details/:id", getDetails);
+router.get(
+  "/get-filter-details/:bedrooms?/:bathrooms?/:houseType?",
+  filterDetails
+);
 
 module.exports = exports = router;
